cd45aec2200be0e20a6de3dc11a34c781caf6a43
Moritz Gebhardt <gebhardt.mo@gmail.com>
2020-05-13 19:54:39 +0200

    [feature] routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
+import { CreatToolComponent } from './tool/creat-tool/creat-tool.component';
+import { OrganisationComponent } from './organisation/organisation.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'organisation', component: OrganisationComponent },
+  { path: 'creat-tool', component: CreatToolComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
