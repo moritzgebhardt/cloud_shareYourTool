ad9bd8bf6b15b915f7d881d748ff54a259e38ad6
Moritz Gebhardt <gebhardt.mo@gmail.com>
2020-05-14 10:34:03 +0200

    [feature] home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,7 +8,12 @@ import { OrganisationComponent } from './organisation/organisation.component';
 import { ToolComponent } from './tool/tool.component';
 import { CreatOrganisationComponent } from './organisation/creat-organisation/creat-organisation.component';
 import { CreatToolComponent } from './tool/creat-tool/creat-tool.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule} from '@angular/material/grid-list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +26,13 @@ import { CreatToolComponent } from './tool/creat-tool/creat-tool.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
