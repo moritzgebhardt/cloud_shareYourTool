09894e3048f486af9bf65a5c57c7f5608b9de93e
Moritz Gebhardt <gebhardt.mo@gmail.com>
2020-05-17 11:55:00 +0200

    change routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
+import { ToolComponent } from './tool/tool.component';
 import { CreatOrganisationComponent } from './organisation/creat-organisation/creat-organisation.component';
 import { OrganisationComponent } from './organisation/organisation.component';
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CreateToolComponent } from './tool/create-tool/create-tool.component';
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'organisation', component: OrganisationComponent },
+  { path: 'tool', component: ToolComponent},
   { path: 'create-tool', component: CreateToolComponent},
   { path: 'create-organisation', component: CreatOrganisationComponent}
 ];
